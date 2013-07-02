f3599229754f079690723fb1d7d1a0ea16b3d7af
Shehzan Devani <shehzandevani@gmail.com>
2013-07-01 23:51:30 -0500

    Initial commit with rails files, gitignore and gemfile update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstBlog::Application.config.secret_token = 'c98abae778917af6a6851e25ea05d585f4118ec75ff20d7f445bceb9fa146cb2b04c289301d0f184cb547a35574f135d0d47d375a49a34fb9389d8da829f2360'
